7264b8b87aca9d4e14f5fc7d0f7543ab1a88b9bf
Julie Kramer <thejuliekramer@gmail.com>
2014-06-17 19:27:47 -0400

    updates to gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,22 @@
 class User < ActiveRecord::Base
 	has_secure_password
+	has_many :cohorts, through: :enrollments
+	has_many :comments
+	has_many :enrollments
+	has_many :submissions
 	belongs_to :school
 
+validates_associated :cohorts, :comments, :enrollment, :submissions
+
+validates :username, uniqueness: {case_sensitive: false},
+			presence: true
+validates :first_name, presence: true, 
+			uniqueness: { case_sensitive: false }, 
+			length: { minimum: 2 }
+validates :last_name, presence: true, 
+			uniqueness: { case_sensitive: false }, 
+			length: { minimum: 2 }
+validates :email, presence: true, 
+			uniqueness: { case_sensitive: false },
+			format: { with: /\A[^@]+@[^@]+\z/ }
 end

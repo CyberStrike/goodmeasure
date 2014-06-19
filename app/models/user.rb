5a585da1db12a2b93ff3fe97e97382def9d800d6
Walter Latimer, Jr <walter@walterlatimer.com>
2014-06-19 01:15:09 -0400

    Generated avatar attribute for Users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,12 @@ class User < ActiveRecord::Base
 	has_many :submissions
 	belongs_to :school
 
+
+
+has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+
 validates_associated :cohorts, :comments, :enrollments, :submissions
 
 validates :username, uniqueness: {case_sensitive: false},

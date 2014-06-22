3a5ffbe9248c96a281ba16a369eac605674cdf51
Bryan <bryan@webphenoms.com>
2014-06-22 15:51:49 -0400

    units tables on cohorts show page works

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -6,7 +6,7 @@ class UnitsController < ApplicationController
   # GET /units
   # GET /units.json
   def index
-    @units = @cohort.units
+    @units = Unit.all
   end
 
   # GET /units/1
@@ -16,7 +16,7 @@ class UnitsController < ApplicationController
 
   # GET /units/new
   def new
-    @unit = @cohort.units.new
+    @unit = Unit.new
   end
 
   # GET /units/1/edit
@@ -66,7 +66,7 @@ class UnitsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_unit
-      @unit = @cohort.units.find(params[:id])
+      @unit = Unit.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.

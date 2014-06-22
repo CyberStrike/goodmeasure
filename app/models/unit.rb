77ec3d2c4f757ef85b5761cdb4c8663bc2f7d4ca
Julie Kramer <thejuliekramer@gmail.com>
2014-06-22 18:16:06 -0400

    fixed unit path, added new task button

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -3,8 +3,10 @@ class Unit < ActiveRecord::Base
   has_many :tasks
 
 
- validates :name, presence: true
- 
- validates :description, presence: true
+ validates :name, 
+ 	presence: true
+
+ validates :description, 
+ 	presence: true
 
 end

8cf8540d99bc19d3e41eab126b8e3d09bb017021
Julie Kramer <thejuliekramer@gmail.com>
2014-06-23 17:13:44 -0400

    fixed unit new button

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -1,6 +1,6 @@
 class UnitsController < ApplicationController
   before_action :set_unit, only: [:show, :edit, :update, :destroy]
-  before_action :set_cohort, only: [:new]
+  before_action :set_cohort, only: [:new, :edit, :update]
   # GET /units
   # GET /units.json
   def index

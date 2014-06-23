f71490bcd84c81409a97712bba97e6b8be20b35a
Bryan <bryan@webphenoms.com>
2014-06-23 18:48:03 -0400

    updated the 'new unit' button on cohort show page

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -2,11 +2,6 @@ class Unit < ActiveRecord::Base
   belongs_to :cohort
   has_many :tasks
 
-
- validates :name, 
- 	presence: true
-
- validates :description, 
- 	presence: true
-
+  validates :name, :description, :cohort_id, presence: true
+ 	
 end

c39cb81aa02f2ebed51448b29ba139066fa8e7d3
Walter Latimer, Jr <walter@walterlatimer.com>
2014-06-24 23:45:19 -0400

    Added validations to enrollments model

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -3,11 +3,7 @@ class Enrollment < ActiveRecord::Base
 	belongs_to :cohort
 	belongs_to :school
 	belongs_to :role
-	validates_uniqueness_of :user, scope: [:cohort]
-	# validates :role, numericality: {
-	#                      only_integer: true,
-	#                      greater_than: 0,
-	#                      less_than_or_equal_to: 5
-	#                    }
+	validates_uniqueness_of :user, scope: [:cohort], message: "is already enrolled in this cohort."
+	validates_presence_of :user, :cohort, :school, :role
 
 end

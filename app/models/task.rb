14eacc7f6e3de2c373502cf8928d26b1bf1b0ac0
Bryan <bryan@webphenoms.com>
2014-06-27 21:21:33 -0400

    added prev and next links for tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,27 +4,20 @@ class Task < ActiveRecord::Base
 	
 	validates :title, presence: true
 
+	def has_next?
+		!unit.tasks.where("id > ?", self.id).order("id ASC").first.blank?
+	end
 
-	# def self.multiple_tasks
-	# 	if self.all > 1
-	# 	end
-	# end
-	
-	# def next
-	# 	if Task.all > 1
-	# 		self.next = 
-	# 	foo = Task.find(params[:id])
-	# end
-
-	# def previous
-	# end
+	def has_previous?
+		!unit.tasks.where("id < ?", self.id).order("id DESC").first.blank?
+	end
 
 	def next
-		unit.tasks.where("id > ?", http://self.id).order("id ASC").first
+		unit.tasks.where("id > ?", self.id).order("id ASC").first
 	end
 	
 	def previous
-		unit.tasks.where("id < ?", http://self.id).order("id DESC").first
+		unit.tasks.where("id < ?", self.id).order("id DESC").first
 	end
 	
 	end

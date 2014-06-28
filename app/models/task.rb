87417af5f9f87be99db894a076f007e498770201
Bryan <bryan@webphenoms.com>
2014-06-27 21:10:56 -0400

    debugging task.next

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,30 @@ class Task < ActiveRecord::Base
 	has_many :submissions, dependent: :destroy
 	
 	validates :title, presence: true
-end
+
+
+	# def self.multiple_tasks
+	# 	if self.all > 1
+	# 	end
+	# end
+	
+	# def next
+	# 	if Task.all > 1
+	# 		self.next = 
+	# 	foo = Task.find(params[:id])
+	# end
+
+	# def previous
+	# end
+
+	def next
+		unit.tasks.where("id > ?", http://self.id).order("id ASC").first
+	end
+	
+	def previous
+		unit.tasks.where("id < ?", http://self.id).order("id DESC").first
+	end
+	
+	end
 
 ## if we wanted to time up to the milisecond it would go in the task model

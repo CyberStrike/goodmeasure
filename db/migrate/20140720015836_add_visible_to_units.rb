a51ae91f836990b8758fa9bb1d43300bfc25b7ef
Bryan <bryan@webphenoms.com>
2014-07-19 21:59:29 -0400

    added Visible column to Units table. added stats.html page

---
+++ db/migrate/20140720015836_add_visible_to_units.rb
@@ -0,0 +1,4 @@
+class AddVisibleToUnits < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140715202209) do
+ActiveRecord::Schema.define(version: 20140720015836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "cohorts", force: true do |t|
     t.string   "name"
@@ -61,6 +67,19 @@ ActiveRecord::Schema.define(version: 20140715202209) do
   end
 
   add_index "invites", ["school_id"], name: "index_invites_on_school_id", using: :btree
+
+  create_table "products", force: true do |t|
+    t.string   "name"
+    t.integer  "price_in_cents"
+    t.integer  "quantity"
+    t.text     "description"
+    t.integer  "category_id"
+    t.boolean  "featured"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
   create_table "related_materials", force: true do |t|
     t.integer  "attachable_id"
